e25a4d1a6567a613acafa42aa693986b85a319c6
reya-naher <reyanaher157@gmail.com>
2020-10-04 01:10:50 +0600

    fake data created

---
+++ src/FakeData/FakeData.js
@@ -0,0 +1,104 @@
+const data = [
+  {
+    id: "1",
+    name: "Child Support",
+    image:"https://i.imgur.com/HCG7iRb.png"
+  },
+  {
+    id: "2",
+    name: "Refuse Shelter",
+    image:"https://i.imgur.com/BBvPRNt.png"
+  },
+  {
+    id: "3",
+    name: "Food Charity",
+    image:"https://i.imgur.com/0DobgwT.png"
+  },
+  {
+    id: "4",
+    name: "Host A Clothing Swap",
+    image:"https://i.imgur.com/aujkJ3c.png"
+  },
+  {
+    id: "5",
+    name: "Host A River Clean-Up",
+    image:"https://i.imgur.com/F7U17XT.png"
+  },
+  {
+    id: "6",
+    name: "Clean Water For Children",
+    image:"https://i.imgur.com/JvvMVd5.png"
+  },
+  {
+    id: "7",
+    name: "Good Education",
+    image:"https://i.imgur.com/VwSvpmq.png"
+  },
+  {
+    id: "8",
+    name: "New Books For Children",
+    image:"https://i.imgur.com/JPm0W4h.png"
+  },
+  {
+    id: "9",
+    name: "Host A Study Group",
+    image:"https://i.imgur.com/atkIKvS.png"
+  },
+  {
+    id: "10",
+    name: "Build Birdhouse For Your Neighbors",
+    image:"https://i.imgur.com/9n19wwq.png"
+  },
+  {
+    id: "11",
+    name: "Organize Books At The Library",
+    image:"https://i.imgur.com/JNIcSZg.png"
+  },
+  {
+    id: "12",
+    name: "Give A Seminar On Driving Safety",
+    image:"https://i.imgur.com/knIwEKS.png"
+  },
+  {
+    id: "13",
+    name: "Give Free Music Lessons",
+    image:"https://i.imgur.com/wPVZKom.png"
+  },
+  {
+    id: "14",
+    name: "Teach People How To Register To Vote",
+    image:"https://i.imgur.com/ynIMcwq.png"
+  },
+  {
+    id: "15",
+    name: "Clean Up Your Local Park",
+    image:"https://i.imgur.com/cCtGj3I.png"
+  },
+  {
+    id: "16",
+    name: "Give IT Help To Local Adults",
+    image:"https://i.imgur.com/4hg7oLh.png"
+  },
+  {
+    id: "17",
+    name: "Foster A Shelter Animal",
+    image:"https://i.imgur.com/O1KEgyi.png"
+  },
+  {
+    id: "18",
+    name: "Babysit During PTA Meetings",
+    image:"https://i.imgur.com/Is0ETtK.png"
+  },
+  {
+    id: "19",
+    name: "Collect Stuffed Animals",
+    image:"https://i.imgur.com/edZwTXX.png"
+  },
+  {
+    id: "20",
+    name: "Collected School Supplies",
+    image:"https://i.imgur.com/3smDXL8.png"
+  }
+
+]
+export default data;
